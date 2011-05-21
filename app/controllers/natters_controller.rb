a0b8df0d6dbf5ddca6c13c412921533d3adab3f3
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2011-05-21 11:14:00 -0400

    now sends messages

--- app/controllers/natters_controller.rb
+++ app/controllers/natters_controller.rb
@@ -50,11 +50,11 @@ class NattersController < ApplicationController
     @schmozzeler = Schmozzeler.find_or_create_by_address clean_field(params[:from])
     @natter = Natter.new(message:params[:text], schmozzeler_id:@schmozzeler.id)
     if @natter.save
-      mail = PostOffice.natter(@schmozzeler.all.map(&:address)-@schmozzeler.address, @natter.message)
+      mail = Postoffice.natter(@schmozzeler.all.map(&:address)-@schmozzeler.address, @natter.message)
       mail.deliver
       render text:"OK"
     else
-      mail = PostOffice.natter(@schmozzeler.address)
+      mail = Postoffice.natter(@schmozzeler.address)
       mail.deliver
       render text:"NG", :status=>:unprocessable_entity
     end
