1607cd6600b530864849ce7b1c7ebc19972f009b
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2011-05-21 11:35:18 -0400

    now sends messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+private
+  def authenticate
+    authenticate_or_request_with_http_basic do |id, password| 
+      id == 'soodesune' && password == 'novirus'
+    end
+  end
+
 end
