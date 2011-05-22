d07b97a6c782e9ba72e05d254e7f2f0ff8f2ff6a
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2011-05-22 17:15:29 -0400

    more commands added

--- app/controllers/natters_controller.rb
+++ app/controllers/natters_controller.rb
@@ -80,7 +80,7 @@ class NattersController < ApplicationController
         when "all_contacts" then Schmozzeler.all.map(&:address).join('\n') 
         when 'help' 
           'call_me - to name yourself\n' +
-          "who's_here - to see the names of the schmozzelers\n"
+          "who's_here - to see the names of the schmozzelers\n" +
           "all_contacts - to see the contact info for all schmozzelers\n"
         else "Sorry, I don\'t know how to '#{command_key}'.  Try, #help"
       end
