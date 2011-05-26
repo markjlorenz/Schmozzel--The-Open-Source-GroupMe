3f03aa2722a968967761aa42c088faff2cb852e9
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2011-05-25 21:07:49 -0400

    threads added

--- app/models/schmozzeler.rb
+++ app/models/schmozzeler.rb
@@ -3,7 +3,6 @@ class Schmozzeler < ActiveRecord::Base
   
   scope :muted, where('muted_at IS NOT NULL')
   scope :listening, where('muted_at IS NULL')
-  scope :in_thread, join(:natters).where('')
 
   def self.in_thread thread
     joins(:natter).where('natters.thread == ?', thread)
