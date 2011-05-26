9926fa7088f94b2446865675546104d9b227bce8
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2011-05-25 21:03:21 -0400

    threads added

--- app/models/schmozzeler.rb
+++ app/models/schmozzeler.rb
@@ -3,7 +3,11 @@ class Schmozzeler < ActiveRecord::Base
   
   scope :muted, where('muted_at IS NOT NULL')
   scope :listening, where('muted_at IS NULL')
+  scope :in_thread, join(:natters).where('')
 
+  def self.in_thread thread
+    join(:natter).where('natters.thread == ?', thread)
+  end
   def rename new_name
     update_attribute :name, new_name
   end
