41a1626351f6f4a563458ddcd09339a997f6e5da
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2011-12-30 15:55:53 -0500

    making public

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'digest'
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate, except:[:mobile, :home]
@@ -5,7 +6,7 @@ class ApplicationController < ActionController::Base
 private
   def authenticate
     authenticate_or_request_with_http_basic do |id, password| 
-      id == 'soodesune' && password == 'novirus'
+      id == 'admin' && Digest::MD5.hexdigest(password) == "75007cbaca213d3df9010b3ef217f176" #hint: no so secure password
     end
   end
 
